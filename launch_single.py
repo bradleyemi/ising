07c89eb320280ac4b5be831e1fac8f615e4b500f
Bradley Emi <bemi@stanford.edu>
2018-05-27 23:30:38 -0700

    add write metrics

--- launch_single.py
+++ launch_single.py
@@ -1,4 +1,5 @@
 import argparse
+import pickle
 import numpy as np
 from utils import simulate_local
 import time
@@ -10,9 +11,12 @@ SAMPLE_RATE = 10
 
 parser = argparse.ArgumentParser()
 parser.add_argument("temperature", help="temperature to run the experiment at", type=float)
+parser.add_argument("outfile", help="name of the place to write the metrics to", type=str)
 args = parser.parse_args()
 
 print("Running simulation for temperature:", args.temperature)
 s = time.time()
 metrics = simulate_local(GRID_SIZE, ITERATIONS_PER_SITE, args.temperature, BURN_IN, SAMPLE_RATE)
+with open(args.outfile, 'wb') as f:
+    pickle.dump(metrics, f)
 print("Simulation took", time.time() - s, "s")
